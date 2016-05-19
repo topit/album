e87a408fdeb331618e924fdfb571ba0c432e0c02
topit <issac360@live.cn>
2016-05-19 15:56:24 +0800

    改变预览时图片大小

--- script/album.js
+++ script/album.js
@@ -5,9 +5,10 @@ for (var i = 0; i < 20; i++) {
   img.src = 'http://o7dn9mux7.bkt.clouddn.com/img' + i + '.jpg';
   pics.push(img);
 }
-pics.sort(function () {
-	return Math.random() > 0.5 ? 1 : -1;
-})
+// pics.sort(function () {
+// 	return Math.random() > 0.5 ? 1 : -1;
+// })
+pics.sort(() => (Math.random() > 0.5 ? 1 : -1));
 //设置图片源
 function setPics() {
 	var pic = new Image();
@@ -17,7 +18,7 @@ function setPics() {
 
 //将图片添加到DOM
 function addPic2DOM(num) {
-	var wrap = document.getElementsByClassName('wrap')[0];
+	var wrap = document.getElementById('wrap');
 	var fragment = document.createDocumentFragment();
 	for (var i = 0; i < num; i++) {
 		var newBox = document.createElement("div");
@@ -43,40 +44,31 @@ function isNeedLoad() {
 	return (lastPicHeight > nowHeight) ? false : true;
 }
 
-function md(e) {
-	var disX = e.clientX - img.offsetLeft,
-			disY = e.clientY - img.offsetTop;
-	document.onmousemove = function (event) {
-		changeX = event.clientX - disX;
-		changeY = event.clientY - disY;	
-		img.style.top = changeY + 100 +'px';
-		img.style.left = changeX + 150 +'px';
-	}
-	document.onmouseup = function () {
-		document.onmousemove = null;
-		document.onmouseup = null;
-	}
-}
-
 //点击图片
 function clickPic(e) {
 	var target = e.target;
-	var mask = document.getElementsByClassName('mask')[0];
+	var mask = document.getElementById('mask');
 	var pic = new Image();
 	pic.src = target.src;
-	// pic.onmousedown = md;
+
+	var screenW = document.documentElement.clientWidth;
+	var screenH = document.documentElement.clientHeight;
+	screenW *= 0.6;
+	screenH *= 0.6;
+	if(pic.height > screenH) {pic.width *= (screenH / pic.height);pic.height = screenH;}
+	if(pic.width > screenW) {pic.height *= (screenW / pic.width);pic.width = screenW;}
+	console.log(`pic.height: ${pic.height}`);
+	console.log(`pic.width: ${pic.width}`);
+
 	mask.innerHTML = "";
 	mask.appendChild(pic);
 	mask.style.display = "flex";
 }
 //将添加到DOM中的图片按照瀑布流布局
-function waterfall(parent,box) {
-  //获取全部box
-  var wrap = document.getElementsByClassName(parent)[0];
-  var boxs = document.getElementsByClassName(box);
-  var screenW = document.documentElement.clientWidth;
+function waterfall(col) {
+  var cols = col || getCols();
+  var boxs = document.getElementsByClassName("box");
   var boxW = boxs[0].offsetWidth;
-  var cols = Math.floor(screenW / boxW);
   wrap.style.width = boxW * cols + "px";
   wrap.style.margin = "0 auto";
   var colHeight = [];
@@ -105,32 +97,38 @@ function getIndex(arr,data) {
   }
 }
 
+//获取当前屏幕可以容纳的列数
+function getCols() {
+	var wrap = document.getElementById("wrap");
+  var boxs = document.getElementsByClassName("box");
+  var screenW = document.documentElement.clientWidth;
+  var boxW = boxs[0].offsetWidth;
+  var cols = Math.floor(screenW / boxW);
+  return cols;
+}
+
 var flag = true;
 window.onload = function () {
 	//判断屏幕变化并改变列数，只有当列数改变时才重新渲染
 	var col;
 	window.onresize = function () {
-		var wrap = document.getElementsByClassName("wrap")[0];
-	  var boxs = document.getElementsByClassName("box");
-	  var screenW = document.documentElement.clientWidth;
-	  var boxW = boxs[0].offsetWidth;
-	  var cols = Math.floor(screenW / boxW);
+	  var cols = getCols();
 		if(col !== cols){
-			col = waterfall("wrap", "box");
+			col = waterfall();
 		}
 	}
 
 	var loader = document.getElementsByClassName('loading')[0];
-	var wrap = document.getElementsByClassName('wrap')[0];
+	var wrap = document.getElementById('wrap');
 	loader.style.display = "none";
 	wrap.style.display = "block";
 	addPic2DOM(20);
-	waterfall("wrap", "box");
+	waterfall();
 	window.onscroll = function () {
 		if(isNeedLoad() && flag){
 			flag = false;
 			addPic2DOM(10);
-			waterfall("wrap", "box");
+			waterfall();
 		}
 	}
 	wrap.addEventListener("click", function (e) {
@@ -138,24 +136,26 @@ window.onload = function () {
 		clickPic(e);
 	},false);
 
-	var mask = document.getElementsByClassName('mask')[0];
+	var mask = document.getElementById('mask');
 	mask.addEventListener("click", function () {
 		mask.style.display = "none";
 	},false);
 
 	// var img = mask.chlidNode[0];
-	function md(e) {
-		var disX = e.clientX - img.offsetLeft,
-				disY = e.clientY - img.offsetTop;
-		document.onmousemove = function (event) {
-			changeX = event.clientX - disX;
-			changeY = event.clientY - disY;	
-			img.style.top = changeY + 100 +'px';
-			img.style.left = changeX + 150 +'px';
-		}
-		document.onmouseup = function () {
-			document.onmousemove = null;
-			document.onmouseup = null;
-		}
-	}
+	// mask.addEventListener('mousedown', md, false);
+	// function md(e) {
+	// 	e = e.target;
+	// 	var disX = e.clientX - img.offsetLeft,
+	// 			disY = e.clientY - img.offsetTop;
+	// 	document.onmousemove = function (event) {
+	// 		changeX = event.clientX - disX;
+	// 		changeY = event.clientY - disY;	
+	// 		img.style.top = changeY + 100 +'px';
+	// 		img.style.left = changeX + 150 +'px';
+	// 	}
+	// 	document.onmouseup = function () {
+	// 		document.onmousemove = null;
+	// 		document.onmouseup = null;
+	// 	}
+	// }
 }
